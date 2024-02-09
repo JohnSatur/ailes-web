8ebf12ebbf7367dcea3b921cf9c59d8b30751708
Juan Carlos <animashorse3@yahoo.com.mx>
2024-02-09 16:20:12 -0600

    Política de privacidad y cookies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
+import { CookiesComponent } from './pages/cookies/cookies.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full'},
     { path: 'home', redirectTo: ''},
     { path: 'about', component: AboutComponent },
+    { path: 'privacy', component: PrivacyComponent },
+    { path: 'cookies', component: CookiesComponent},
     { path: '**', redirectTo: ''}
 ];
